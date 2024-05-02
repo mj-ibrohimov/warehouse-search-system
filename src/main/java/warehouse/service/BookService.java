b41c9af7693bfa4c931b308262493fc2cff293b2
Muhammadjon Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-05-02 23:36:23 +0500

    Updated UserDao with write and delete functionality, incorporating improvements from read functionality commit

--- src/main/java/warehouse/service/BookService.java
+++ src/main/java/warehouse/service/BookService.java
@@ -4,7 +4,11 @@ import warehouse.config.CustomFileReader;
 import warehouse.domains.User;
 import warehouse.domains.UserRole;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,5 +38,30 @@ public class UserDao implements BaseDao<User> {
                 .phoneNumber(strings[4])
                 .role(UserRole.valueOf(strings[5]))
                 .build();
+    }
+
+    // Updated functionality from the first commit:
+    public void saveUserCsvFile(User user) {
+        try (FileWriter fileWriter = new FileWriter(userFile, true)) {
+            fileWriter.write(user.getId() + "," + user.getUsername() + "," +
+                    user.getPassword() + "," + user.getFullName() + "," + user.getPhoneNumber() + "," + user.getRole() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeUser(Long userId) {
+        List<String> lines = null;
+        Path path = Paths.get(userFile);
+        try {
+            lines = fileReader.readFile(userFile);
+            List<String> modifiedLines = new ArrayList<>();
+            List<String> strings = List.of("id,username,password,fullName,phoneNumber,role");
+            modifiedLines.addAll(strings);
+            modifiedLines.addAll(lines.stream().filter(line -> !line.startsWith(userId + ",")).toList());
+            Files.write(path, modifiedLines);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
