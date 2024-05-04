632e859166f7f95b3bb853998cdbeb729ec2fb16
Muhammadjon Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-05-04 17:52:27 +0500

    Added MessageHelper interface for defining common message constants

---
+++ src/main/java/warehouse/domains/MessageHelper.java
@@ -0,0 +1,19 @@
+package warehouse.domains;
+
+public interface MessageHelper {
+    String CREATED_USER = "Created account";
+    String WRONG_OPTION = "Wrong option";
+    String USER_NOT_FOUND = "User not found";
+    String LOGIN = "Successfully Login";
+    String USER_ALERTEST = "User already exist";
+    String BOOK_NOT_FOUND = "Book not found!";
+    String POSTER_NOT_FOUND = "Poster not found!";
+    String BOOK_DELETED = "Book deleted successfully";
+    String BOOK_SAVED = "Book saved successfully";
+    String POSTER_SAVED = "Poster saved successfully";
+    String MAGAZINE_SAVED = "Magazine saved successfully";
+    String POSTER_DELETED = "Poster deleted successfully";
+    String MAGAZINE_DELETED = "Magazine deleted successfully";
+
+    String MAGAZINE_NOT_FOUND = "Magazine not found!";
+}
