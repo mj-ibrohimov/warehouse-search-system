23fd8095448d22158d4ee69fefe733c4c8805439
Muhammadjon Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-05-06 15:26:42 +0500

    Add ResponseEntity for encapsulating response data

---
+++ src/main/java/warehouse/dtos/ResponseEntity.java
@@ -0,0 +1,18 @@
+package warehouse.dtos;
+
+import lombok.Getter;
+
+@Getter
+public class ResponseEntity<T>{
+    private final T data;
+    private Integer status;
+
+    public ResponseEntity(T data) {
+        this.data = data;
+    }
+
+    public ResponseEntity(T data, Integer status) {
+        this.data = data;
+        this.status = status;
+    }
+}
