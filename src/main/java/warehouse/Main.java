9a705a72f45e13c289ddc2ca1a08601a27760c7f
Muhammadjon Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-05-16 21:49:12 +0500

    debugged the user.csv file and made it possible to sign in with admin role and user role. In each role, there will be different options to implement.

--- src/main/java/warehouse/Main.java
+++ src/main/java/warehouse/Main.java
@@ -6,14 +6,18 @@ import warehouse.utils.BaseUtils;
 import java.io.IOException;
 
 public class Main {
-    static AppUI appUI=new AppUI();
-    public static void main(String[] args) throws IOException {
+    static AppUI appUI = new AppUI();
 
-        BaseUtils.println("\n\n*************** Project: Printed Product Warehouse *****************");
-        BaseUtils.println("--------------- Author: Muhammadjon Ibrohimov  ---------------");
-        BaseUtils.println("--------------- Email: Muhammadjon_Ibrohimov@student.itpu.uz ---------------");
-        BaseUtils.println("--------------- Creation starting date: since 07.03.2024 ---------------");
-        BaseUtils.println("--------------- Version: version-0.0.1 ---------------");
+    public static void main(String[] args) throws IOException {
+        printBanner();
         appUI.run();
+    }
+
+    private static void printBanner() {
+        BaseUtils.println("\n\n\033[0;36m*************** Project: Printed Product Warehouse *****************\033[0m");
+        BaseUtils.println("\033[0;33m--------------- Author: Muhammadjon Ibrohimov  ---------------\033[0m");
+        BaseUtils.println("\033[0;33m--------------- Email: Muhammadjon_Ibrohimov@student.itpu.uz ---------------\033[0m");
+        BaseUtils.println("\033[0;33m--------------- Creation starting date: since 07.03.2024 ---------------\033[0m");
+        BaseUtils.println("\033[0;33m--------------- Version: version-0.0.1 ---------------\033[0m");
     }
 }
